57f4fed7d3dd0b5c4bc1d2a44a0d438016ebbfe2
swafe <svenfachinger@web.de>
2020-06-15 12:43:24 +0200

    add utils_script

---
+++ utils_scripts/exiftool_change_orienntation.ps1
@@ -0,0 +1,6 @@
+﻿#https://gitlab.com/vgg/via/-/issues/145
+cd C:\Users\sven\Desktop\test
+#Examples
+exiftool -Orientation=Horizontal Img_5288.jpg
+#-overwrite_original
+exiftool -Orientation=Horizontal -overwrite_original Img_5288.jpg
